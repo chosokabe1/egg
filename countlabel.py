c27d3f7278aa176f4c12308684e1e64418c18c5a
chosokabe1 <tktkshi101110@gmail.com>
2022-02-27 12:53:49 +0900

    countlabel-kai

--- countlabel.py
+++ countlabel.py
@@ -25,30 +25,31 @@ files = glob.glob(inpath + "/*.txt")
 for fname in files:
     with open(fname)as f:
         for line in f.readlines():
-            if line[0:2] == '0 ':
+            split = line.split()
+            if split[0] == '0':
                 counter_0 += 1
             
-            elif line[0:2] == '1 ':
+            elif split[0] == '1':
                 counter_1 += 1
-            elif line[0:2] == '2 ':
+            elif split[0] == '2':
                 counter_2 += 1
-            elif line[0:2] == '3 ':
+            elif split[0] == '3':
                 counter_3 += 1
-            elif line[0:2] == '4 ':
+            elif split[0] == '4':
                 counter_4 += 1
-            elif line[0:2] == '5 ':
+            elif split[0] == '5':
                 counter_5 += 1
-            elif line[0:2] == '6 ':
+            elif split[0] == '6':
                 counter_6 += 1
-            elif line[0:2] == '7 ':
+            elif split[0] == '7':
                 counter_7 += 1
-            elif line[0:2] == '8 ':
+            elif split[0] == '8':
                 counter_8 += 1
-            elif line[0:2] == '9 ':
+            elif split[0] == '9':
                 counter_9 += 1
-            elif line[0:3] == '10 ':
+            elif split[0] == '10':
                 counter_10 += 1
-            elif line[0:3] == '11 ':
+            elif split[0] == '11':
                 counter_11 += 1
             else:
                 counter_else += 1
